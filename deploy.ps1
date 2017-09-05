f46d7d8c972b88ce1c6d70b285ec66d041fc946d
chantisnake <13501393281@163.com>
2017-09-05 13:07:22 -0400

    deploying

--- deploy.ps1
+++ deploy.ps1
@@ -8,10 +8,13 @@ param (
              ParameterSetName="deploymentPath",
              ValueFromPipelineByPropertyName=$true,
              HelpMessage="Literal path to one or more locations.")]
-  [Alias("PSPath")]
+  [Alias("Path")]
   [ValidateNotNullOrEmpty()]
-  [string]
-  $deploymentPath
+  [string[]]
+  $deploymentPaths,
+
+  [switch]
+  $noGitSync
 )
 
 $ErrorActionPreference = "stop"
@@ -19,52 +22,71 @@ Import-Module Posh-SSH
 
 # remote names
 $remoteComputerName = "cs.wheatoncollege.edu"
-$remoteUserName = "czhang"
+
+# set the path of the file to store user name and password
+$usernamePath = "$PSScriptRoot/username.secret"
+$passwordPath = "$PSScriptRoot/pass.secret"
+
+# get the user name and password from file
+if (Test-Path $usernamePath)
+{
+  $remoteUserName = Get-Content -Path $usernamePath
+}
+else
+{
+  $remoteUserName = Read-Host "please input the username on the server"
+  $remoteUserName | Out-File $usernamePath -Encoding utf8
+}
+if (Test-Path $passwordPath)
+{
+  $password = Get-Content -Path $passwordPath
+}
+else
+{
+  $password = Read-Host "please input your password on the server"
+  $password | Out-File $passwordPath -Encoding utf8
+}
+
 # make credential
-$password = Get-Content -Path "$PSScriptRoot/pass.secret"
 $securePass =ConvertTo-SecureString $password -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential ($remoteUserName, $securePass)
 
 # commiting and pushing to git
-Write-Host "commiting and pushing to git" -ForegroundColor Yellow
-Start-Process "git" -ArgumentList "commit -am `"deploying $deploymentPath`"" -NoNewWindow -Wait
-Start-Process "git" -ArgumentList "push" -NoNewWindow -Wait
-Write-Host "push successful" -ForegroundColor Green
-Write-Host
-Write-Host
+if (-not $noGitSync)
+{
+  # add git ignore file to ignore your password and username file
+  if ( -not (Test-Path "$PSScriptRoot\.gitignore"))
+  {
+    "*.secret" | Out-File -FilePath "$PSScriptRoot\.gitignore" -Encoding utf8
+  }
+
+  Write-Host "commiting and pushing to git" -ForegroundColor Yellow
+  Start-Process "git" -ArgumentList "commit -am `"deploying $deploymentPath`"" -NoNewWindow -Wait
+  Start-Process "git" -ArgumentList "push" -NoNewWindow -Wait
+  Write-Host "push successful" -ForegroundColor Green
+  Write-Host
+  Write-Host
+}
 
 # connecting
 Write-Host "connecting to remote" -ForegroundColor Yellow
 Write-Verbose "remote computer name: $remoteComputerName"
 Write-Verbose "remote user name:  $remoteUserName"
 Write-Verbose "password: $password"
-$session = New-SSHSession -ComputerName $remoteComputerName -Credential $credential
+$session = New-SFTPSession -ComputerName $remoteComputerName -Credential $credential
 Write-Host "connection successful" -ForegroundColor Green
 Write-Host
 Write-Host
 
-
-# helper function
-function Invoke-RemoteCommand ($command)
-{
-
-  Write-Verbose "excuting command: $command"
-  $responce = Invoke-SSHCommand -SSHSession $session -Command $command
-
-  $responce.output | ForEach-Object {Write-Host $_ -ForegroundColor Gray}
-  $responce.error | ForEach-Object {Write-Host $_ -ForegroundColor Red}
-
-  Write-Verbose "exit code: $($responce.ExitStatus)"
-}
-
-# update the repo
-Write-Host "updating remote repo" -ForegroundColor Yellow
-Invoke-RemoteCommand -command "cd ~/COMP298/ && git pull"
+Write-Host "pushing file to the server" -ForegroundColor Yellow
+Set-SFTPFile -SFTPSession $seesion -RemotePath 'www' -LocalFile $deploymentPaths -Overwrite
+Write-Host "push sucess" -ForegroundColor Green
+Write-Host
 Write-Host
 
 # End connection
 Write-Host "trying to close remote connection" -ForegroundColor Yellow
-$success = Remove-SSHSession -SSHSession $session
+$success = Remove-SFTPSession -SSHSession $session
 if ($success)
 {
   Write-Host "connection closed successfully, exiting" -ForegroundColor Green
