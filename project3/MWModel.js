43507cc0a37a3477ae4f4218db6297a5b968c603
chantisnake <13501393281@163.com>
2017-10-23 10:46:56 -0400

    remove all the generated file

--- project3/MWModel.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=MWModel.js.map
--- project3/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./canvasModel");
-require("./EModel");
-$(document).ready(function () {
-});
-//# sourceMappingURL=index.js.map
