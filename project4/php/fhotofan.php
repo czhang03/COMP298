5fb73d84727725c1e6109c1a8713708a9c9e2164
chantisnake <13501393281@163.com>
2017-12-01 17:10:41 -0500

    add a secure login php

---
+++ project4/php/fhotofan.php
@@ -0,0 +1,40 @@
+<?php
+
+class response
+{
+    public $success;
+    public $username;
+    public $imageList;
+}
+
+class image
+{
+    public $yearTaken;
+    public $country;
+    public $state;
+    public $location;
+    public $description;
+}
+
+echo json_encode([
+    "success" => true,
+    "username" => "Joe",
+    "imageList" => [
+        [
+            "srcUrl" => "https://pbs.twimg.com/profile_images/469017630796296193/R-bEN4UP.png",
+            "yearTaken" => 1960,
+            "country" => "China",
+            "state" => "",
+            "location" => "this \" <>",
+            "description" => "this is character test quote and slash: \" ' \ \nthis is on new line"
+        ],
+        [
+            "srcUrl" => "http://blogs.edweek.org/edweek/the_startup_blog/Test%20Sign.jpg",
+            "yearTaken" => 2016,
+            "country" => "United State",
+            "state" => "MA",
+            "location" => "common park",
+            "description" => "good day!"
+        ]
+    ]
+]);
--- project4/php/login.php
+++ project4/php/login.php
@@ -1,41 +1,59 @@
 <?php
 
-class response
-{
-    public $success;
-    public $username;
-    public $imageList;
+const login_data_file = "database/login.json";
+const secure_key_path = "key.secrete";
+
+$username = $_GET["username"];
+$password = $_GET["password"];
+$password_hash = hash("sha256", $password);
+
+$json_str = file_get_contents(login_data_file);
+$login_database = json_decode($json_str, true);
+
+// helper function to write content to file path
+function write_to_file($content, $file_path) {
+    $Handle = fopen($file_path, 'w');
+    fwrite($Handle, $content);
 }
 
-class image
-{
-    public $yearTaken;
-    public $country;
-    public $state;
-    public $location;
-    public $description;
+// get the secrete key on this server
+function get_secrete_key() {
+    if (file_exists(secure_key_path)) {
+        return file_get_contents(secure_key_path);
+    }
+    else {
+        $secure_key = bin2hex(random_bytes(16));
+        write_to_file($secure_key, secure_key_path);
+        return $secure_key;
+    }
 }
 
-echo json_encode([
-    "success" => true,
-    "username" => "Joe",
-    "imageList" => [
-        [
-            "srcUrl" => "https://pbs.twimg.com/profile_images/469017630796296193/R-bEN4UP.png",
-            "yearTaken" => 1960,
-            "country" => "China",
-            "state" => "",
-            "location" => "this \" <>",
-            "description" => "this is character test quote and slash: \" ' \ \nthis is on new line"
-    ],
-        [
-            "srcUrl" => "http://blogs.edweek.org/edweek/the_startup_blog/Test%20Sign.jpg",
-            "yearTaken" => 2016,
-            "country" => "United State",
-            "state" => "MA",
-            "location" => "common park",
-            "description" => "good day!"
-        ]
-    ]
-]);
+// authenticate the user
+if (array_key_exists($username, $login_database)) {
 
+    // login successful
+    if (strcmp($login_database[$username]["passwordSha256"], $password_hash) == 0) {
+
+        // generate the secure session token
+        $currentTime = time();
+        $secure_token = json_encode([
+            "time" => $currentTime,
+            "username" => $username,
+            "Hash" => hash("sha256", $currentTime . $username . get_secrete_key())
+        ]);
+
+        // put the secure token in session
+        $_SESSION["token"] = $secure_token;
+
+        echo json_encode(["success" => true]);
+    }
+
+    // password error
+    else {
+        echo json_encode(["success" => false, "error" => "wrong password"]);
+    }
+}
+
+else {
+    echo json_encode(["success" => false, "error" => "username not found"]);
+}
