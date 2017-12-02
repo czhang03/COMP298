2bb590ee14765716096dde85d6de0853d0f9ecd0
chantisnake <13501393281@163.com>
2017-12-02 13:21:09 -0500

    update the login to use the token handler

--- project4/php/login.php
+++ project4/php/login.php
@@ -1,5 +1,7 @@
 <?php
 
+include "token_handler.php";
+
 // start the session
 session_start();
 
@@ -21,41 +23,14 @@ else {
 
 $login_database = json_decode($json_str, true);
 
-// helper function to write content to file path
-function write_to_file($content, $file_path) {
-    $Handle = fopen($file_path, 'w');
-    fwrite($Handle, $content);
-}
-
-// get the secrete key on this server
-function get_secrete_key() {
-    $secure_key_path = sys_get_temp_dir() . "/key.secret";
-    if (file_exists($secure_key_path)) {
-        return file_get_contents($secure_key_path);
-    }
-    else {
-        $secure_key = bin2hex(random_bytes(16));
-        write_to_file($secure_key, $secure_key_path);
-        return $secure_key;
-    }
-}
-
 // authenticate the user
 if (array_key_exists($username, $login_database)) {
 
     // login successful
     if (strcmp($login_database[$username]["passwordSha256"], $password_hash) == 0) {
 
-        // generate the secure session token
-        $currentTime = time();
-        $secure_token = json_encode([
-            "time" => $currentTime,
-            "username" => $username,
-            "Hash" => hash("sha256", $currentTime . $username . get_secrete_key())
-        ]);
-
-        // put the secure token in session
-        $_SESSION["token"] = $secure_token;
+        // generate the secure token
+        set_new_secure_token($username);
 
         echo json_encode(["success" => true]);
     }
