bb291f32d5219e79f049c8438d94aaea315506d9
chantisnake <13501393281@163.com>
2017-12-02 12:22:26 -0500

    add a more realistic backend to fotofan

--- project4/php/fotofan.php
+++ project4/php/fotofan.php
@@ -1,23 +1,13 @@
 <?php
+
+$user_id = "Joe";
+
+$image_data_path = "../database/" . $user_id . ".json";
+
+$image_data = json_decode(file_get_contents($image_data_path), true);
+
 echo json_encode([
     "success" => true,
-    "username" => "Joe",
-    "imageList" => [
-        [
-            "srcUrl" => "https://pbs.twimg.com/profile_images/469017630796296193/R-bEN4UP.png",
-            "yearTaken" => 1960,
-            "country" => "China",
-            "state" => "",
-            "location" => "this \" <>",
-            "description" => "this is character test quote and slash: \" ' \ \nthis is on new line"
-        ],
-        [
-            "srcUrl" => "http://blogs.edweek.org/edweek/the_startup_blog/Test%20Sign.jpg",
-            "yearTaken" => 2016,
-            "country" => "United State",
-            "state" => "MA",
-            "location" => "common park",
-            "description" => "good day!"
-        ]
-    ]
+    "username" => "Uncle Joe Mikey",
+    "imageList" => $image_data
 ]);
