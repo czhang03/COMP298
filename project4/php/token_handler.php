666f6cf74bcaf982b776bda873429595c7a47895
chantisnake <13501393281@163.com>
2017-12-02 13:16:32 -0500

    add the token handler to handle token

---
+++ project4/php/token_handler.php
@@ -0,0 +1,103 @@
+<?php
+// common functions that are used in multiple file
+
+const hash_algorithm = "sha256";  // the algorithm used for hashing
+const token_key = "token";  // the key of the token in session
+
+// get the secrete key on this server
+function get_secrete_key() {
+    $secure_key_path = sys_get_temp_dir() . "/key.secret";
+    if (file_exists($secure_key_path)) {
+        return file_get_contents($secure_key_path);
+    }
+    else {
+        $secure_key = bin2hex(random_bytes(16));
+        file_put_contents($secure_key_path, $secure_key);
+        return $secure_key;
+    }
+}
+
+function _generate_token($current_time, $username) {
+    // generate the secure session token
+    return json_encode([
+        "time" => $current_time,
+        "username" => $username,
+        "hash" => hash(hash_algorithm, $current_time . $username . get_secrete_key())
+    ]);
+}
+
+function set_new_secure_token($username) {
+    $current_time = time();
+    $_SESSION[token_key] = _generate_token($current_time, $username);
+}
+
+function destroy_token() {
+    // destroy the old session
+    session_destroy();
+
+    // start the new session
+    session_start();
+}
+
+// the time when token is outdated
+const token_valid_period_in_hour = 1;
+
+// helper function for validate token, to return true if token is not outdated
+function _token_not_outdated($time_created) {
+    // calculate whether this session is outdated
+    $current_time = time();
+    $time_passed_in_hour = abs($current_time - $time_created) / 360; // convert seconds to hours
+    return $time_passed_in_hour > token_valid_period_in_hour;
+}
+
+// helper function for validate token, validate the hash is correct
+function _token_hash_correct($time_created, $username, $hash) {
+    // validate the hash is correct
+    $expected_hash = hash(hash_algorithm, $time_created . $username . get_secrete_key());
+    return strcmp($hash, $expected_hash) === 1;
+}
+
+// validate the session token
+// session token is a json string of the form:
+// {
+//    "time": timeCreated,
+//    "username": username,
+//    "hash": sha256(timeCreated + username + secrete_key)
+// }
+function _validate_token($token_str) {
+    $token_obj = json_decode($token_str, true);
+
+    // unpack
+    $time_created = $token_obj["time"];
+    $username = $token_obj["username"];
+    $hash = $token_obj["hash"];
+
+    return _token_not_outdated($time_created) && _token_hash_correct($time_created, $username, $hash);
+}
+
+// validate the token from session super global
+function _validate_token_from_session() {
+    // if token do not exists
+    if (!isset($_SESSION[token_key]))
+        return false;
+
+    // validate the token
+    return _validate_token($_SESSION[token_key]);
+}
+
+
+// validate the token
+// if validated, then update the token and return true.
+// if not validated, then clear the token and return false.
+function validate_and_update($username) {
+    // if token is not valid
+    if (! _validate_token_from_session()) {
+        destroy_token();
+        return false;
+    }
+    // if the token is valid
+    else {
+        set_new_secure_token($username);
+        return true;
+    }
+}
