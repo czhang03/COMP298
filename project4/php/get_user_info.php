3d759e496b08f0c5a4b09ecc204e83b37cf3c7ab
chantisnake <13501393281@163.com>
2017-12-03 13:26:16 -0500

    if get the user info safely

--- project4/php/get_user_info.php
+++ project4/php/get_user_info.php
@@ -1,6 +1,9 @@
 <?php
 include "user_info_handler.php";
 
+// authenticate the user first
+precede_if_token_valid();
+
 // this file tries to get the user info form token
 $username = get_user_name_from_cookie_token();
 
