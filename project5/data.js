ebd4256503aff4ff6979370c50632eaf98f06d9a
chantisnake <13501393281@163.com>
2017-12-06 22:52:01 -0500

    add description

--- project5/data.js
+++ project5/data.js
@@ -1,23 +1,38 @@
 const PizzaData = {
   0: {
     id: 0,
-    name: "doge pizza",
-    price: 1
+    name: 'doge pizza',
+    price: 1,
+    descriptionHTML:
+    'Such Pizza. So Cheese.<br>' +
+    'Much Oven. Just Wow.'
   },
   1: {
     id: 1,
-    name: "cs pizza",
-    price: 89
+    name: 'cs pizza',
+    price: 89,
+    descriptionHTML:
+    'Baked with luxurious 5 dollar oven, <br>' +
+    'by very skilled computer science undergrad.<br>' +
+    'Trust me, I ate a lot of pizza.'
   },
   2: {
     id: 2,
-    name: "Mikey Pizza",
-    price: 65
+    name: 'Mikey Pizza',
+    price: 65,
+    descriptionHTML:
+    'Baked with luxurious 5 dollar oven, <br>' +
+    'by very skilled computer science undergrad.<br>' +
+    'Trust me, I ate a lot of pizza.'
   },
   3: {
     id: 3,
-    name: "Imaginary Pizza",
+    name: 'Imaginary Pizza',
     price: 0,
-
+    descriptionHTML:
+    "Carb free, vegetarian friendly and gluten free.<br>" +
+    "Even cheese free, topping free, and flour free.<br>" +
+    "You can buy it for free, we can sell it for free, <br>" +
+    "the most free pizza in the world."
   }
 }
