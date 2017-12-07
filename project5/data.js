02a586bd6dac0da0ffe19ab6283bf9a42d44951b
chantisnake <13501393281@163.com>
2017-12-06 22:03:58 -0500

    add basic data

---
+++ project5/data.js
@@ -0,0 +1,23 @@
+const PizzaData = {
+  0: {
+    id: 0,
+    name: "doge pizza",
+    price: 1
+  },
+  1: {
+    id: 1,
+    name: "cs pizza",
+    price: 89
+  },
+  2: {
+    id: 2,
+    name: "Mikey Pizza",
+    price: 65
+  },
+  3: {
+    id: 3,
+    name: "Imaginary Pizza",
+    price: 0,
+
+  }
+}
