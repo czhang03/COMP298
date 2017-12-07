2319da790def74e94ed9c64c872f7bdeef9567d9
chantisnake <13501393281@163.com>
2017-12-07 15:24:27 -0500

    add the user greeting

--- project5/index.js
+++ project5/index.js
@@ -1,3 +1,12 @@
+function greeting () {
+  if (checkAndSetExistingUser())
+    Materialize.toast("Welcome back, old chump", 4000, "toast-center")
+  else
+    Materialize.toast("Greetings, stranger, welcome to the palace", 4000, "toast-center")
+}
+
 $(() => {
   $('.slider').slider({interval: 10000})
+
+  greeting()
 })
