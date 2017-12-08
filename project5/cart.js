0156af1e0f66ca0cd656c9eaa680b7c2c373cf69
chantisnake <13501393281@163.com>
2017-12-08 18:12:06 -0500

    add basic styling for order list

--- project5/cart.js
+++ project5/cart.js
@@ -1,19 +1,22 @@
-function getEmptyOrderHTML() {
+function getEmptyOrderHTML () {
   return `<img  src="img/csSlide.png">`
 }
 
-function getSingleOrderHTML(orderObj) {
+function getSingleOrderHTML (orderObj) {
   return `
 <div class="order-div">
     <img src="${orderObj.pizzaData.image}" alt="${orderObj.pizzaData.name}" class="order-image">
     <div class="order-name">${orderObj.pizzaData.name}</div>
-    <form><input type="number" class="order-count-number" value="${orderObj.orderCount}"></form>
-    <button class="order-delete-button">Delete</button>
+    <form class="order-action">
+      <label for="order-number-${orderObj.pizzaData.id}">Order Number</label>
+      <input type="number" id="order-number-${orderObj.pizzaData.id}" class="order-count-number" value="${orderObj.orderCount}">
+      <input type="button" class="order-delete-button" value="Delete">
+     </form>
 </div>
   `
 }
 
-function getOrderListHTML() {
+function getOrderListHTML () {
   const orderList = getOrderObjectLists()
   if (orderList.length === 0)
     return getEmptyOrderHTML()
@@ -21,18 +24,17 @@ function getOrderListHTML() {
     return orderList.map(getSingleOrderHTML)
 }
 
-
 /**
  * update the subsection of delivery time
  */
-function updateSubTime() {
-  if ($("#time-later").prop("checked")) {
-    $(".sub-time").hide()
-    $("#sub-time-later").show()
+function updateSubTime () {
+  if ($('#time-later').prop('checked')) {
+    $('.sub-time').hide()
+    $('#sub-time-later').show()
   }
-  else if ($("#time-now").prop("checked")) {
-    $(".sub-time").hide()
-    $("#sub-time-now").show()
+  else if ($('#time-now').prop('checked')) {
+    $('.sub-time').hide()
+    $('#sub-time-now').show()
   }
 }
 
@@ -40,10 +42,10 @@ $(() => {
 
   // update the sub-time element
   updateSubTime()
-  $(".delivery-time-radio").change(updateSubTime)
+  $('.delivery-time-radio').change(updateSubTime)
 
   // init date time picker
-  $(".timepicker").pickatime({
+  $('.timepicker').pickatime({
     default: 'now', // Set default time: 'now', '1:30AM', '16:30'
     fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
     twelvehour: false, // Use AM/PM or 24-hour format
@@ -52,7 +54,7 @@ $(() => {
     canceltext: 'Cancel', // Text for cancel-button
     autoclose: false, // automatic close timepicker
     ampmclickable: true, // make AM PM clickable
-  });
+  })
   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15, // Creates a dropdown of 15 years to control year,
@@ -60,8 +62,8 @@ $(() => {
     clear: 'Clear',
     close: 'Ok',
     closeOnSelect: false // Close upon selecting a date,
-  });
+  })
 
   // load all the orders
-  $("#order-list").html(getOrderListHTML())
+  $('#order-list').html(getOrderListHTML())
 })
