250347fc8395df615df01901ad7f9c95867695ad
chantisnake <13501393281@163.com>
2017-12-08 17:34:32 -0500

    add order list

--- project5/cart.js
+++ project5/cart.js
@@ -1,3 +1,30 @@
+function getEmptyOrderHTML() {
+  return `<img  src="img/csSlide.png">`
+}
+
+function getSingleOrderHTML(orderObj) {
+  return `
+<div class="order-div">
+    <img src="${orderObj.pizzaData.image}" alt="${orderObj.pizzaData.name}" class="order-image">
+    <div class="order-name">${orderObj.pizzaData.name}</div>
+    <form><input type="number" class="order-count-number" value="${orderObj.orderCount}"></form>
+    <button class="order-delete-button">Delete</button>
+</div>
+  `
+}
+
+function getOrderListHTML() {
+  const orderList = getOrderObjectLists()
+  if (orderList.length === 0)
+    return getEmptyOrderHTML()
+  else
+    return orderList.map(getSingleOrderHTML)
+}
+
+
+/**
+ * update the subsection of delivery time
+ */
 function updateSubTime() {
   if ($("#time-later").prop("checked")) {
     $(".sub-time").hide()
@@ -10,6 +37,8 @@ function updateSubTime() {
 }
 
 $(() => {
+
+  // update the sub-time element
   updateSubTime()
   $(".delivery-time-radio").change(updateSubTime)
 
@@ -32,4 +61,7 @@ $(() => {
     close: 'Ok',
     closeOnSelect: false // Close upon selecting a date,
   });
+
+  // load all the orders
+  $("#order-list").html(getOrderListHTML())
 })
