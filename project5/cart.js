c2bf106c743c21657ab53e4605ff6783740fda7f
chantisnake <13501393281@163.com>
2017-12-08 20:45:01 -0500

    add the address modal

--- project5/cart.js
+++ project5/cart.js
@@ -97,4 +97,18 @@ $(() => {
   // load the order list
   updateOrderListHtml()
 
+  // ================= place order section ================
+  $('.modal').modal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      inDuration: 300, // Transition in duration
+      outDuration: 200, // Transition out duration
+      startingTop: '4%', // Starting top style attribute
+      endingTop: '10%', // Ending top style attribute
+    }
+  );
+
+  $("#place-order-button").click(() => {
+    $("#address-modal").modal("open")
+  })
 })
